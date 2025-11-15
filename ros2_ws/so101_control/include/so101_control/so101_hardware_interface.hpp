8c98172349a6014f91911f46ee6e533a0bb784f9
shrujan <shrujanus@gmail.com>
2025-11-15 17:44:15 +0000

    Clang-formated-files

--- ros2_ws/so101_control/include/so101_control/so101_hardware_interface.hpp
+++ ros2_ws/so101_control/include/so101_control/so101_hardware_interface.hpp
@@ -1,33 +1,36 @@
 #ifndef SO101_HARDWARE_INTERFACE_HPP
 #define SO101_HARDWARE_INTERFACE_HPP
 
-#include "SMS_STS.h"
 #include <cmath>
 #include <string>
 #include <vector>
 
+#include "SMS_STS.h"
 #include "hardware_interface/handle.hpp"
 #include "hardware_interface/hardware_info.hpp"
 #include "hardware_interface/system_interface.hpp"
 #include "hardware_interface/types/hardware_interface_return_values.hpp"
+#include "pluginlib/class_list_macros.hpp"
 #include "rclcpp/macros.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp_lifecycle/node_interfaces/lifecycle_node_interface.hpp"
 #include "rclcpp_lifecycle/state.hpp"
 
-#include "pluginlib/class_list_macros.hpp"
-
 using hardware_interface::CallbackReturn;
 
-namespace so101_control {
+namespace so101_control
+{
 
-struct Servo {
+struct Servo
+{
   Servo() = default;
-  Servo(int servo_id) : id(servo_id) {}
+  Servo(int servo_id) : id(servo_id)
+  {
+  }
   int id;
   double position_offset;
 
-  double cmd_id; // 0 = position, 1 = velocity
+  double cmd_id;  // 0 = position, 1 = velocity
   double cmd_position;
   double cmd_velocity;
 
@@ -38,8 +41,8 @@ struct Servo {
   double voltage_state;
 };
 
-class so101_hardware_interface final
-    : public hardware_interface::SystemInterface {
+class so101_hardware_interface final : public hardware_interface::SystemInterface
+{
 private:
   SMS_STS motor_driver_;
   std::string usb_port_ = "/dev/ttyUSB0";
@@ -53,33 +56,22 @@ public:
   so101_hardware_interface();
 
   // Lifecycle methods
-  CallbackReturn
-  on_configure(const rclcpp_lifecycle::State &previous_state) override;
-  CallbackReturn
-  on_activate(const rclcpp_lifecycle::State &previous_state) override;
-  CallbackReturn
-  on_deactivate(const rclcpp_lifecycle::State &previous_state) override;
-  CallbackReturn
-  on_cleanup(const rclcpp_lifecycle::State &previous_state) override;
-  CallbackReturn
-  on_shutdown(const rclcpp_lifecycle::State &previous_state) override;
-  CallbackReturn
-  on_error(const rclcpp_lifecycle::State &previous_state) override;
+  CallbackReturn on_configure(const rclcpp_lifecycle::State& previous_state) override;
+  CallbackReturn on_activate(const rclcpp_lifecycle::State& previous_state) override;
+  CallbackReturn on_deactivate(const rclcpp_lifecycle::State& previous_state) override;
+  CallbackReturn on_cleanup(const rclcpp_lifecycle::State& previous_state) override;
+  CallbackReturn on_shutdown(const rclcpp_lifecycle::State& previous_state) override;
+  CallbackReturn on_error(const rclcpp_lifecycle::State& previous_state) override;
   // Export interfaces
-  CallbackReturn
-  on_init(const hardware_interface::HardwareInfo &params) override;
-  std::vector<hardware_interface::StateInterface>
-  export_state_interfaces() override;
-  std::vector<hardware_interface::CommandInterface>
-  export_command_interfaces() override;
+  CallbackReturn on_init(const hardware_interface::HardwareInfo& params) override;
+  std::vector<hardware_interface::StateInterface> export_state_interfaces() override;
+  std::vector<hardware_interface::CommandInterface> export_command_interfaces() override;
 
   // Read/Write methods
-  hardware_interface::return_type read(const rclcpp::Time &time,
-                                       const rclcpp::Duration &period) override;
-  hardware_interface::return_type
-  write(const rclcpp::Time &time, const rclcpp::Duration &period) override;
+  hardware_interface::return_type read(const rclcpp::Time& time, const rclcpp::Duration& period) override;
+  hardware_interface::return_type write(const rclcpp::Time& time, const rclcpp::Duration& period) override;
 };
 
-} // namespace so101_control
+}  // namespace so101_control
 
 #endif
--- ros2_ws/so101_control/src/so101_hardware_interface.cpp
+++ ros2_ws/so101_control/src/so101_hardware_interface.cpp
@@ -1,61 +1,61 @@
 #include "so101_control/so101_hardware_interface.hpp"
 
-so101_control::so101_hardware_interface::so101_hardware_interface() {
+so101_control::so101_hardware_interface::so101_hardware_interface()
+{
   // Constructor implementation (if needed)
 }
 
 hardware_interface::CallbackReturn
-so101_control::so101_hardware_interface::on_init(
-    const hardware_interface::HardwareInfo &params) {
-  if (hardware_interface::SystemInterface::on_init(params) !=
-      hardware_interface::CallbackReturn::SUCCESS) {
+so101_control::so101_hardware_interface::on_init(const hardware_interface::HardwareInfo& params)
+{
+  if (hardware_interface::SystemInterface::on_init(params) != hardware_interface::CallbackReturn::SUCCESS)
+  {
     return hardware_interface::CallbackReturn::ERROR;
   }
   auto it = params.hardware_parameters.find("usb_port");
-  if (it != params.hardware_parameters.end()) {
+  if (it != params.hardware_parameters.end())
+  {
     usb_port_ = it->second;
   }
   auto baud_it = params.hardware_parameters.find("baud_rate");
-  if (baud_it != params.hardware_parameters.end()) {
+  if (baud_it != params.hardware_parameters.end())
+  {
     baud_rate_ = std::stoi(baud_it->second);
   }
   num_joints_ = params.joints.size();
   servos_.resize(num_joints_);
-  RCLCPP_INFO(rclcpp::get_logger("so101_hardware_interface"),
-              "Initializing hardware interface for %d joints.", num_joints_);
+  RCLCPP_INFO(rclcpp::get_logger("so101_hardware_interface"), "Initializing hardware interface for %d joints.",
+              num_joints_);
 
   num_joints_ = params.joints.size();
-  std::unordered_set<std::string> joint_names, state_interface_names,
-      command_interface_names;
+  std::unordered_set<std::string> joint_names, state_interface_names, command_interface_names;
 
-  for (int i = 0; i < num_joints_; ++i) {
-
-    if (joint_names.find(params.joints[i].name) != joint_names.end()) {
-      RCLCPP_FATAL(rclcpp::get_logger("so101_hardware_interface"),
-                   "Joint name %s is not unique.",
+  for (int i = 0; i < num_joints_; ++i)
+  {
+    if (joint_names.find(params.joints[i].name) != joint_names.end())
+    {
+      RCLCPP_FATAL(rclcpp::get_logger("so101_hardware_interface"), "Joint name %s is not unique.",
                    params.joints[i].name.c_str());
       return hardware_interface::CallbackReturn::ERROR;
     }
 
-    if (params.joints[i].state_interfaces.size() != 5) {
-      RCLCPP_FATAL(rclcpp::get_logger("so101_hardware_interface"),
-                   "Joint %s does not have 5 state interfaces.",
+    if (params.joints[i].state_interfaces.size() != 5)
+    {
+      RCLCPP_FATAL(rclcpp::get_logger("so101_hardware_interface"), "Joint %s does not have 5 state interfaces.",
                    params.joints[i].name.c_str());
       return hardware_interface::CallbackReturn::ERROR;
     }
     state_interface_names.clear();
 
-    for (const auto &state_interface : params.joints[i].state_interfaces) {
-      if (state_interface_names.find(state_interface.name) !=
-              state_interface_names.end() ||
-          (state_interface.name != "position" &&
-           state_interface.name != "velocity" &&
-           state_interface.name != "torque" &&
-           state_interface.name != "temperature" &&
-           state_interface.name != "voltage")) {
+    for (const auto& state_interface : params.joints[i].state_interfaces)
+    {
+      if (state_interface_names.find(state_interface.name) != state_interface_names.end() ||
+          (state_interface.name != "position" && state_interface.name != "velocity" &&
+           state_interface.name != "torque" && state_interface.name != "temperature" &&
+           state_interface.name != "voltage"))
+      {
         RCLCPP_FATAL(rclcpp::get_logger("so101_hardware_interface"),
-                     "Joint %s has duplicate/unknown state interface %s.",
-                     params.joints[i].name.c_str(),
+                     "Joint %s has duplicate/unknown state interface %s.", params.joints[i].name.c_str(),
                      state_interface.name.c_str());
         return hardware_interface::CallbackReturn::ERROR;
       }
@@ -63,82 +63,81 @@ so101_control::so101_hardware_interface::on_init(
       state_interface_names.insert(state_interface.name);
     }
 
-    if (params.joints[i].command_interfaces.size() != 2) {
-      RCLCPP_FATAL(rclcpp::get_logger("so101_hardware_interface"),
-                   "Joint %s does not have 2 command interfaces.",
+    if (params.joints[i].command_interfaces.size() != 2)
+    {
+      RCLCPP_FATAL(rclcpp::get_logger("so101_hardware_interface"), "Joint %s does not have 2 command interfaces.",
                    params.joints[i].name.c_str());
       return hardware_interface::CallbackReturn::ERROR;
     }
     command_interface_names.clear();
-    for (const auto &command_interface : params.joints[i].command_interfaces) {
-      if (command_interface_names.find(command_interface.name) !=
-          command_interface_names.end()) {
-        RCLCPP_FATAL(rclcpp::get_logger("so101_hardware_interface"),
-                     "Joint %s has duplicate command interface %s.",
-                     params.joints[i].name.c_str(),
-                     command_interface.name.c_str());
+    for (const auto& command_interface : params.joints[i].command_interfaces)
+    {
+      if (command_interface_names.find(command_interface.name) != command_interface_names.end())
+      {
+        RCLCPP_FATAL(rclcpp::get_logger("so101_hardware_interface"), "Joint %s has duplicate command interface %s.",
+                     params.joints[i].name.c_str(), command_interface.name.c_str());
         return hardware_interface::CallbackReturn::ERROR;
       }
       command_interface_names.insert(command_interface.name);
     }
     servos_[i] = so101_control::Servo(i + 1);
     auto offset_it = params.joints[i].parameters.find("position_offset");
-    if (offset_it != params.joints[i].parameters.end()) {
+    if (offset_it != params.joints[i].parameters.end())
+    {
       servos_[i].position_offset = std::stod(offset_it->second);
     }
-    servos_[i].cmd_id =
-        params.joints[i].parameters.at("type") == "position" ? 0.0 : 1.0;
+    servos_[i].cmd_id = params.joints[i].parameters.at("type") == "position" ? 0.0 : 1.0;
   }
   return hardware_interface::CallbackReturn::SUCCESS;
 }
 
 hardware_interface::CallbackReturn
-so101_control::so101_hardware_interface::on_configure(
-    const rclcpp_lifecycle::State & /*previous_state*/) {
-
-  if (!motor_driver_.begin(baud_rate_, usb_port_.c_str())) {
-    RCLCPP_FATAL(rclcpp::get_logger("so101_hardware_interface"),
-                 "Failed to open motor driver on port: %s", usb_port_.c_str());
+so101_control::so101_hardware_interface::on_configure(const rclcpp_lifecycle::State& /*previous_state*/)
+{
+  if (!motor_driver_.begin(baud_rate_, usb_port_.c_str()))
+  {
+    RCLCPP_FATAL(rclcpp::get_logger("so101_hardware_interface"), "Failed to open motor driver on port: %s",
+                 usb_port_.c_str());
     return hardware_interface::CallbackReturn::ERROR;
   }
-  RCLCPP_INFO(rclcpp::get_logger("so101_hardware_interface"),
-              "Successfully connected to motor driver on port: %s",
+  RCLCPP_INFO(rclcpp::get_logger("so101_hardware_interface"), "Successfully connected to motor driver on port: %s",
               usb_port_.c_str());
 
   // Ping each servo to ensure they are connected
-  for (int i = 0; i < num_joints_; ++i) {
-    if (motor_driver_.Ping(servos_[i].id) == -1) {
-      RCLCPP_FATAL(rclcpp::get_logger("so101_hardware_interface"),
-                   "Failed to ping motor ID %d.", servos_[i].id);
+  for (int i = 0; i < num_joints_; ++i)
+  {
+    if (motor_driver_.Ping(servos_[i].id) == -1)
+    {
+      RCLCPP_FATAL(rclcpp::get_logger("so101_hardware_interface"), "Failed to ping motor ID %d.", servos_[i].id);
       return hardware_interface::CallbackReturn::ERROR;
     }
   }
-  RCLCPP_INFO(rclcpp::get_logger("so101_hardware_interface"),
-              "Successfully pinged all motor IDs.");
+  RCLCPP_INFO(rclcpp::get_logger("so101_hardware_interface"), "Successfully pinged all motor IDs.");
 
   // setting up each servo
-  for (int i = 0; i < num_joints_; ++i) {
-    motor_driver_.Mode(servos_[i].id, 0); // position control mode
+  for (int i = 0; i < num_joints_; ++i)
+  {
+    motor_driver_.Mode(servos_[i].id, 0);  // position control mode
   }
   return hardware_interface::CallbackReturn::SUCCESS;
 }
 
 hardware_interface::CallbackReturn
-so101_control::so101_hardware_interface::on_cleanup(
-    const rclcpp_lifecycle::State & /*previous_state*/) {
+so101_control::so101_hardware_interface::on_cleanup(const rclcpp_lifecycle::State& /*previous_state*/)
+{
   motor_driver_.end();
-  RCLCPP_INFO(rclcpp::get_logger("so101_hardware_interface"),
-              "Motor driver connection closed.");
+  RCLCPP_INFO(rclcpp::get_logger("so101_hardware_interface"), "Motor driver connection closed.");
   return hardware_interface::CallbackReturn::SUCCESS;
 }
 
 hardware_interface::CallbackReturn
-so101_control::so101_hardware_interface::on_activate(
-    const rclcpp_lifecycle::State & /*previous_state*/) {
-  RCLCPP_INFO(rclcpp::get_logger("so101_hardware_interface"),
-              "Activating hardware interface for %d joints.", num_joints_);
+so101_control::so101_hardware_interface::on_activate(const rclcpp_lifecycle::State& /*previous_state*/)
+{
+  RCLCPP_INFO(rclcpp::get_logger("so101_hardware_interface"), "Activating hardware interface for %d joints.",
+              num_joints_);
 
-  for (int i = 0; i < num_joints_; ++i) {
+  for (int i = 0; i < num_joints_; ++i)
+  {
     double initial_pose = servos_[i].position_state;
     servos_[i].cmd_position = initial_pose;
     servos_[i].cmd_velocity = 0.0;
@@ -147,18 +146,17 @@ so101_control::so101_hardware_interface::on_activate(
   return hardware_interface::CallbackReturn::SUCCESS;
 }
 
-hardware_interface::CallbackReturn
-so101_control::so101_hardware_interface::update_hardware_status(int id) {
-
-  if (motor_driver_.FeedBack(id) == -1) {
-    RCLCPP_ERROR(rclcpp::get_logger("so101_hardware_interface"),
-                 "Failed to get feedback from motor ID %d.", id);
+hardware_interface::CallbackReturn so101_control::so101_hardware_interface::update_hardware_status(int id)
+{
+  if (motor_driver_.FeedBack(id) == -1)
+  {
+    RCLCPP_ERROR(rclcpp::get_logger("so101_hardware_interface"), "Failed to get feedback from motor ID %d.", id);
     return hardware_interface::CallbackReturn::ERROR;
-  } else {
-    servos_[id - 1].position_state =
-        motor_driver_.ReadPos(id) * 2 * M_PI / steps_per_rev_;
-    servos_[id - 1].velocity_state =
-        motor_driver_.ReadSpeed(id) * 2 * M_PI / steps_per_rev_;
+  }
+  else
+  {
+    servos_[id - 1].position_state = motor_driver_.ReadPos(id) * 2 * M_PI / steps_per_rev_;
+    servos_[id - 1].velocity_state = motor_driver_.ReadSpeed(id) * 2 * M_PI / steps_per_rev_;
     servos_[id - 1].torque_state = motor_driver_.ReadLoad(id);
     servos_[id - 1].voltage_state = motor_driver_.ReadVoltage(id);
     servos_[id - 1].temperature_state = motor_driver_.ReadTemper(id);
@@ -167,64 +165,70 @@ so101_control::so101_hardware_interface::update_hardware_status(int id) {
 }
 
 hardware_interface::CallbackReturn
-so101_control::so101_hardware_interface::on_deactivate(
-    const rclcpp_lifecycle::State & /*previous_state*/) {
-  RCLCPP_INFO(rclcpp::get_logger("so101_hardware_interface"),
-              "Deactivating hardware interface for %d joints.", num_joints_);
-  for (int i = 0; i < num_joints_; ++i) {
+so101_control::so101_hardware_interface::on_deactivate(const rclcpp_lifecycle::State& /*previous_state*/)
+{
+  RCLCPP_INFO(rclcpp::get_logger("so101_hardware_interface"), "Deactivating hardware interface for %d joints.",
+              num_joints_);
+  for (int i = 0; i < num_joints_; ++i)
+  {
     motor_driver_.EnableTorque(i + 1, 0);
   }
   this->write(rclcpp::Time(), rclcpp::Duration(0, 0));
   return hardware_interface::CallbackReturn::SUCCESS;
 }
 
-std::vector<hardware_interface::StateInterface>
-so101_control::so101_hardware_interface::export_state_interfaces() {
+std::vector<hardware_interface::StateInterface> so101_control::so101_hardware_interface::export_state_interfaces()
+{
   std::vector<hardware_interface::StateInterface> state_interfaces;
-  for (int i = 0; i < num_joints_; ++i) {
-    state_interfaces.emplace_back(hardware_interface::StateInterface(
-        info_.joints[i].name, "position", &servos_[i].position_state));
-    state_interfaces.emplace_back(hardware_interface::StateInterface(
-        info_.joints[i].name, "velocity", &servos_[i].velocity_state));
-    state_interfaces.emplace_back(hardware_interface::StateInterface(
-        info_.joints[i].name, "torque", &servos_[i].torque_state));
-    state_interfaces.emplace_back(hardware_interface::StateInterface(
-        info_.joints[i].name, "temperature", &servos_[i].temperature_state));
-    state_interfaces.emplace_back(hardware_interface::StateInterface(
-        info_.joints[i].name, "voltage", &servos_[i].voltage_state));
+  for (int i = 0; i < num_joints_; ++i)
+  {
+    state_interfaces.emplace_back(
+        hardware_interface::StateInterface(info_.joints[i].name, "position", &servos_[i].position_state));
+    state_interfaces.emplace_back(
+        hardware_interface::StateInterface(info_.joints[i].name, "velocity", &servos_[i].velocity_state));
+    state_interfaces.emplace_back(
+        hardware_interface::StateInterface(info_.joints[i].name, "torque", &servos_[i].torque_state));
+    state_interfaces.emplace_back(
+        hardware_interface::StateInterface(info_.joints[i].name, "temperature", &servos_[i].temperature_state));
+    state_interfaces.emplace_back(
+        hardware_interface::StateInterface(info_.joints[i].name, "voltage", &servos_[i].voltage_state));
   }
   return state_interfaces;
 }
 
-std::vector<hardware_interface::CommandInterface>
-so101_control::so101_hardware_interface::export_command_interfaces() {
+std::vector<hardware_interface::CommandInterface> so101_control::so101_hardware_interface::export_command_interfaces()
+{
   std::vector<hardware_interface::CommandInterface> command_interfaces;
-  for (int i = 0; i < num_joints_; ++i) {
-    command_interfaces.emplace_back(hardware_interface::CommandInterface(
-        info_.joints[i].name, "position", &servos_[i].cmd_position));
-    command_interfaces.emplace_back(hardware_interface::CommandInterface(
-        info_.joints[i].name, "velocity", &servos_[i].cmd_velocity));
+  for (int i = 0; i < num_joints_; ++i)
+  {
+    command_interfaces.emplace_back(
+        hardware_interface::CommandInterface(info_.joints[i].name, "position", &servos_[i].cmd_position));
+    command_interfaces.emplace_back(
+        hardware_interface::CommandInterface(info_.joints[i].name, "velocity", &servos_[i].cmd_velocity));
   }
   return command_interfaces;
 }
 
-hardware_interface::return_type so101_control::so101_hardware_interface::read(
-    const rclcpp::Time & /*time*/, const rclcpp::Duration & /*period*/) {
-  for (int i = 1; i <= num_joints_; ++i) {
-    if (update_hardware_status(i) ==
-        hardware_interface::CallbackReturn::ERROR) {
+hardware_interface::return_type so101_control::so101_hardware_interface::read(const rclcpp::Time& /*time*/,
+                                                                              const rclcpp::Duration& /*period*/)
+{
+  for (int i = 1; i <= num_joints_; ++i)
+  {
+    if (update_hardware_status(i) == hardware_interface::CallbackReturn::ERROR)
+    {
       RCLCPP_ERROR(rclcpp::get_logger("so101_hardware_interface"),
-                   "Failed to get feedback from motor ID %d during read.",
-                   servos_[i].id);
+                   "Failed to get feedback from motor ID %d during read.", servos_[i].id);
       return hardware_interface::return_type::ERROR;
     }
   }
   return hardware_interface::return_type::OK;
 }
 
-hardware_interface::return_type so101_control::so101_hardware_interface::write(
-    const rclcpp::Time & /*time*/, const rclcpp::Duration & /*period*/) {
-  for (int i = 0; i < num_joints_; ++i) {
+hardware_interface::return_type so101_control::so101_hardware_interface::write(const rclcpp::Time& /*time*/,
+                                                                               const rclcpp::Duration& /*period*/)
+{
+  for (int i = 0; i < num_joints_; ++i)
+  {
     int id = servos_[i].id;
     s16 position = static_cast<s16>(servos_[i].cmd_position);
     s16 speed = static_cast<s16>(servos_[i].cmd_velocity);
@@ -233,5 +237,4 @@ hardware_interface::return_type so101_control::so101_hardware_interface::write(
   return hardware_interface::return_type::OK;
 }
 
-PLUGINLIB_EXPORT_CLASS(so101_control::so101_hardware_interface,
-                       hardware_interface::SystemInterface)
+PLUGINLIB_EXPORT_CLASS(so101_control::so101_hardware_interface, hardware_interface::SystemInterface)
